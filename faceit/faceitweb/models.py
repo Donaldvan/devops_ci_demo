1a51cc78077970950492a65d15c2d0cd90337a08
Ben Lambert <ben.lambert@cloudacademy.com>
2016-05-26 12:25:21 -0400

    minor adjustments

--- faceit/faceitweb/models.py
+++ faceit/faceitweb/models.py
@@ -1,6 +1,9 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.template.defaultfilters import slugify
+
+from name_tools import canonicalize
 
 class User(models.Model):
     full_name = models.CharField(max_length=70)
@@ -9,3 +12,12 @@ class User(models.Model):
 
     def __unicode__(self):              # __unicode__ on Python 2
         return self.full_name
+
+    def uniform_name(self):
+        return canonicalize(self.full_name)
+
+    def capitalize(self):
+        return self.full_name.upper()
+
+    def slug(self):
+        return slugify(self.full_name)
--- faceit/faceitweb/tests.py
+++ faceit/faceitweb/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
+from .models import User
 
-class RouteTest(TestCase):
+class HomeIntegrationTest(TestCase):
 
     def test_index(self):
         response = self.client.get('/')
@@ -9,3 +10,19 @@ class RouteTest(TestCase):
     def test_index_contains_text(self):
         response = self.client.get('/')
         self.assertTrue('coming soon' in response.content.lower())
+
+
+
+class UserUnitTest(TestCase):
+
+    def setUp(self):
+        self.user = User(full_name="john smith")
+
+    def test_has_uniform_name(self):
+        self.assertEqual(self.user.uniform_name(), "John Smith")
+
+    def test_capitalized_name(self):
+        self.assertEqual(self.user.capitalize(), "JOHN SMITH")
+
+    def test_capitalized_name(self):
+        self.assertEqual(self.user.slug(), "john-smith")
